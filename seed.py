35b1d46e311bdee2d6c89d42edf4d7c001623b7a
Gabby <gvadai91@icloud.com>
2025-12-18 14:29:36 -0500

    updating seed.py for admin user addition

--- seed.py
+++ seed.py
@@ -1,8 +1,25 @@
-from artwork import db
-from app import app
+from artwork import db, AdminUser
+from app import app, bcrypt
+from sqlalchemy import select
+
 
 def main():
     with app.app_context():
         db.create_all()
+
+        existing_admin = db.session.execute (select(AdminUser)).where(AdminUser.email == "robert.w.smith07@gmail.com").scalar()
+
+        if not existing_admin:
+            hashed_pwd = bcrypt.generate_password_hash("1LovePotatoes").decode('utf-8')
+
+            new_admin = AdminUser(
+                email = "robert.w.smith07@gmail.com",
+                password = hashed_pwd,
+                name = 'Danielle'
+            )
+            db.session.add(new_admin)
+            db.session.commit()
+
+
 if __name__ == "__main__":        
     main()
